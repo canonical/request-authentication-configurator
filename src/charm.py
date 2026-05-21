ab65fca8e3d0df7c5d547ebfb5193062dca9b959
MattiaSarti <nmattia1@gmail.com>
2026-05-21 12:38:11 +0200

    fix: handle leadership

--- src/charm.py
+++ src/charm.py
@@ -19,6 +19,7 @@ class RequestAuthenticationIntegratorCharm(ops.CharmBase):
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
         framework.observe(self.on.config_changed, self._on_config_changed)
+        framework.observe(self.on.leader_elected, self._on_leader_elected)
 
     def _on_config_changed(self, event: ops.ConfigChangedEvent):
         """Handle config-changed event."""
@@ -26,12 +27,23 @@ class RequestAuthenticationIntegratorCharm(ops.CharmBase):
             f"config change detected, new value for '{CONFIG_KEY_FOR_USER_ID_HEADER_NAME}': "
             f"{self.user_id_header_name}"
         )
+        self._update_status()
+
+    def _on_leader_elected(self, event: ops.LeaderElectedEvent):
+        """Handle leader-elected event."""
+        self._update_status()
+
+    def _update_status(self):
+        """Update unit and application status."""
         if self.is_user_id_header_name_valid:
             self.unit.status = ops.ActiveStatus()
+            if self.unit.is_leader():
+                self.app.status = ops.ActiveStatus()
         else:
-            self.unit.status = ops.BlockedStatus(
-                f"invalid config value for '{CONFIG_KEY_FOR_USER_ID_HEADER_NAME}'"
-            )
+            message = f"invalid config value for '{CONFIG_KEY_FOR_USER_ID_HEADER_NAME}'"
+            self.unit.status = ops.BlockedStatus(message)
+            if self.unit.is_leader():
+                self.app.status = ops.BlockedStatus(message)
 
     @property
     def is_user_id_header_name_valid(self) -> bool:
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -18,25 +18,36 @@ def compose_charm_configs(user_id_header_name: str) -> dict[str, str]:
 
 
 @pytest.mark.parametrize(
-    "new_configs, are_expected_to_be_valid",
+    "new_configs, are_new_configs_expected_to_be_valid, is_unit_leader",
     [
-        (compose_charm_configs("kubeflow-userid"), True),
-        (compose_charm_configs(""), False),
-        (compose_charm_configs("mlflow-userid"), True),
+        (compose_charm_configs("kubeflow-userid"), True, True),
+        (compose_charm_configs("mlflow-userid"), True, True),
+        (compose_charm_configs(""), False, True),
+        (compose_charm_configs("kubeflow-userid"), True, False),
+        (compose_charm_configs("mlflow-userid"), True, False),
+        (compose_charm_configs(""), False, False),
     ]
 )
-def test_unit_status_based_on_whether_config_change_valid(new_configs, are_expected_to_be_valid):
-    """Test that the charm has the correct state after handling the config-changed event."""
+def test_app_and_unit_status_based_on_leadership_and_whether_config_change_valid(
+    new_configs, are_new_configs_expected_to_be_valid, is_unit_leader
+):
+    """Test that the charm has the correct unit and app statuses after config-changed events."""
     # Arrange:
     ctx = testing.Context(RequestAuthenticationIntegratorCharm, config=None)
 
     # Act:
-    state_in = testing.State(config=new_configs)  # state associated to the event, not initial one
+    state_in = testing.State(config=new_configs, leader=is_unit_leader)
     state_out = ctx.run(ctx.on.config_changed(), state_in)
 
     # Assert:
-    assert state_out.unit_status == (
-        testing.ActiveStatus()
-        if are_expected_to_be_valid else
-        testing.BlockedStatus(f"invalid config value for '{CONFIG_KEY_FOR_USER_ID_HEADER_NAME}'")
-    )
+    if are_new_configs_expected_to_be_valid:
+        assert state_out.unit_status == testing.ActiveStatus()
+        assert state_out.app_status == (
+            testing.ActiveStatus() if is_unit_leader else testing.UnknownStatus()
+        )
+    else:
+        expected_message = f"invalid config value for '{CONFIG_KEY_FOR_USER_ID_HEADER_NAME}'"
+        assert state_out.unit_status == testing.BlockedStatus(expected_message)
+        assert state_out.app_status == (
+            testing.BlockedStatus(expected_message) if is_unit_leader else testing.UnknownStatus()
+        )
