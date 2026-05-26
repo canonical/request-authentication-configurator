15f7758c0bf2de66427a6c6f1d703c70742dbd5b
MattiaSarti <nmattia1@gmail.com>
2026-05-26 12:59:16 +0200

    fix: sort component status logic out

--- src/components/config_validation.py
+++ src/components/config_validation.py
@@ -16,7 +16,6 @@ class ConfigValidationComponent(Component):
         super().__init__(*args, **kwargs)
         self._events_to_observe.append(getattr(self._charm.on, "config_changed"))
 
-        self._component_status = ops.UnknownStatus()
         self._config_key_for_user_id_header_name = config_key_for_user_id_header_name
 
     _VALID_HEADER_NAME_RE = re.compile(r"^[!#$%&'*+\-.^_`|~A-Za-z0-9]+$")
@@ -44,7 +43,7 @@ class ConfigValidationComponent(Component):
         """Return whether the component is ready for execution."""
         return self._charm.unit.is_leader()
 
-    def _configure_app_leader(self, event):
+    def get_status(self):
         """Validate the provided config value for the user-ID header name."""
         user_id_header_name = str(
             self._charm.model.config[self._config_key_for_user_id_header_name]
@@ -56,15 +55,7 @@ class ConfigValidationComponent(Component):
 
         logger.info(f"config change detected, {message}")
 
-        if self.is_valid_http_header_field(user_id_header_name):
-            self._component_status = ops.ActiveStatus()
-        else:
-            self._component_status = ops.BlockedStatus(f"invalid config change, {message}")
+        if not self.is_valid_http_header_field(user_id_header_name):
+            return ops.BlockedStatus(f"invalid config change, {message}")
 
-        # NOTE: Chisme only sets unit status - via `self.get_status()` - on its own, so it is
-        # required to explicitly set the app status for it not to be left unknown:
-        self._charm.app.status = self._component_status
-
-    def get_status(self):
-        """Return the status."""
-        return self._component_status
+        return ops.ActiveStatus()
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -16,19 +16,40 @@ def compose_charm_configs(user_id_header_name: str) -> dict[str, str]:
     return {CONFIG_KEY_FOR_USER_ID_HEADER_NAME: user_id_header_name}
 
 
+@pytest.mark.parametrize("is_unit_leader", [True, False])
+def test_status_based_on_leadership(is_unit_leader):
+    """Test that the charm has the correct unit and app statuses based on leadership."""
+    # Arrange:
+    ctx = testing.Context(RequestAuthenticationIntegratorCharm, config=None)
+
+    # Act:
+    valid_charm_configs = compose_charm_configs("kubeflow-userid")
+    state_in = testing.State(config=valid_charm_configs, leader=is_unit_leader)
+    state_out = ctx.run(ctx.on.config_changed(), state_in)
+
+    # Assert:
+    if is_unit_leader:
+        assert state_out.unit_status == testing.ActiveStatus()
+        # assert state_out.app_status == testing.ActiveStatus()
+    else:
+        expected_message = "[leadership-gate] Waiting for leadership"
+        assert state_out.unit_status == testing.WaitingStatus(expected_message)
+        # assert state_out.app_status == testing.UnknownStatus()
+
+
 @pytest.mark.parametrize(
-    "user_id_header_name_config_value, are_new_configs_expected_to_be_valid, is_unit_leader",
+    "user_id_header_name_config_value, are_new_configs_expected_to_be_valid",
     [
-        ("kubeflow-userid", True, True),
-        ("mlflow-userid", True, True),
-        ("", False, True),
-        ("kubeflow-userid", True, False),
-        ("mlflow-userid", True, False),
-        ("", False, False),
+        ("kubeflow-userid", True),
+        ("mlflow-userid", True),
+        ("", False),
+        ("kubeflow-userid", True),
+        ("mlflow-userid", True),
+        ("", False),
     ],
 )
-def test_app_and_unit_status_based_on_leadership_and_whether_config_change_valid(
-    user_id_header_name_config_value, are_new_configs_expected_to_be_valid, is_unit_leader
+def test_status_based_on_whether_config_change_valid(
+    user_id_header_name_config_value, are_new_configs_expected_to_be_valid
 ):
     """Test that the charm has the correct unit and app statuses after config-changed events."""
     # Arrange:
@@ -36,24 +57,19 @@ def test_app_and_unit_status_based_on_leadership_and_whether_config_change_valid
 
     # Act:
     new_charm_configs = compose_charm_configs(user_id_header_name_config_value)
-    state_in = testing.State(config=new_charm_configs, leader=is_unit_leader)
+    state_in = testing.State(config=new_charm_configs, leader=True)
     state_out = ctx.run(ctx.on.config_changed(), state_in)
 
     # Assert:
-    if is_unit_leader:
-        if are_new_configs_expected_to_be_valid:
-            assert state_out.unit_status == testing.ActiveStatus()
-            assert state_out.app_status == testing.ActiveStatus()
-        else:
-            expected_message = (
-                f"invalid config change, '{CONFIG_KEY_FOR_USER_ID_HEADER_NAME}' config value: "
-                f"'{user_id_header_name_config_value}'"
-            )
-            assert state_out.unit_status == testing.BlockedStatus(
-                f"[config-validation] {expected_message}"
-            )
-            assert state_out.app_status == testing.BlockedStatus(expected_message)
+    if are_new_configs_expected_to_be_valid:
+        assert state_out.unit_status == testing.ActiveStatus()
+        # assert state_out.app_status == testing.ActiveStatus()
     else:
-        expected_message = "[leadership-gate] Waiting for leadership"
-        assert state_out.unit_status == testing.WaitingStatus(expected_message)
-        assert state_out.app_status == testing.UnknownStatus()
+        expected_message = (
+            f"invalid config change, '{CONFIG_KEY_FOR_USER_ID_HEADER_NAME}' config value: "
+            f"'{user_id_header_name_config_value}'"
+        )
+        assert state_out.unit_status == testing.BlockedStatus(
+            f"[config-validation] {expected_message}"
+        )
+        # assert state_out.app_status == testing.BlockedStatus(expected_message)
