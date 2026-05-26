1920fedd223ac6bed7de4fd8b79229aa61816fcf
MattiaSarti <nmattia1@gmail.com>
2026-05-26 15:33:41 +0200

    test: update integration tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -15,9 +15,15 @@ logger = logging.getLogger(__name__)
 METADATA = yaml.safe_load(pathlib.Path("metadata.yaml").read_text())
 CHARM_NAME = METADATA["name"]
 APPLICATION_NAME = "-".join(("my", CHARM_NAME))
+CONFIG_KEY_FOR_USER_ID_HEADER_NAME = "user-id-header-name"
 
 
 def test_deploy(charm: pathlib.Path, juju: jubilant.Juju):
     """Deploy the charm under test."""
-    juju.deploy(charm.resolve(), app=APPLICATION_NAME, resources={})
+    juju.deploy(
+        charm.resolve(),
+        app=APPLICATION_NAME,
+        resources={},
+        config={CONFIG_KEY_FOR_USER_ID_HEADER_NAME: "kubeflow-userid"},
+    )
     juju.wait(jubilant.all_active)
