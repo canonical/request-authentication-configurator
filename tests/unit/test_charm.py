0b3b1bb63182cecd92204620fe57882f31ace75d
MattiaSarti <nmattia1@gmail.com>
2026-05-27 11:19:58 +0200

    test: make unit tests rely on mocks to isolate test-case specific logic

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -3,6 +3,8 @@
 #
 # To learn more about testing, see https://documentation.ubuntu.com/ops/latest/explanation/testing/
 
+from unittest.mock import MagicMock, patch
+
 import pytest
 from ops import testing
 
@@ -17,14 +19,23 @@ def compose_charm_configs(user_id_header_name: str) -> dict[str, str]:
 
 
 @pytest.mark.parametrize("is_unit_leader", [True, False])
-def test_unit_status_based_on_leadership(is_unit_leader):
+@patch("components.config_validation.ConfigValidationComponent.get_status")
+@patch("components.request_auth_integration.RequestAuthRequirerComponent.get_status")
+@patch("components.request_auth_integration.RequestAuthRequirerComponent._configure_app_leader")
+def test_unit_status_based_on_leadership(
+    _: MagicMock,
+    mock_request_auth_integration_get_status: MagicMock,
+    mock_config_validation_get_status: MagicMock,
+    is_unit_leader,
+):
     """Test that the charm has the correct unit status based on leadership."""
     # Arrange:
+    mock_config_validation_get_status.return_value = testing.ActiveStatus()
+    mock_request_auth_integration_get_status.return_value = testing.ActiveStatus()
     ctx = testing.Context(RequestAuthenticationIntegratorCharm, config=None)
 
     # Act:
-    valid_charm_configs = compose_charm_configs("kubeflow-userid")
-    state_in = testing.State(config=valid_charm_configs, leader=is_unit_leader)
+    state_in = testing.State(leader=is_unit_leader)
     state_out = ctx.run(ctx.on.config_changed(), state_in)
 
     # Assert:
@@ -46,11 +57,17 @@ def test_unit_status_based_on_leadership(is_unit_leader):
         ("kubeflow:userid", False),
     ],
 )
+@patch("components.request_auth_integration.RequestAuthRequirerComponent.get_status")
+@patch("components.request_auth_integration.RequestAuthRequirerComponent._configure_app_leader")
 def test_unit_status_based_on_whether_config_change_valid(
-    user_id_header_name_config_value, are_new_configs_expected_to_be_valid
+    _: MagicMock,
+    mock_request_auth_integration_get_status: MagicMock,
+    user_id_header_name_config_value,
+    are_new_configs_expected_to_be_valid,
 ):
     """Test that the charm has the correct unit status after config-changed events."""
     # Arrange:
+    mock_request_auth_integration_get_status.return_value = testing.ActiveStatus()
     ctx = testing.Context(RequestAuthenticationIntegratorCharm, config=None)
 
     # Act:
