93608c905d553c619f9a8cba405d9ab3b73b9188
MattiaSarti <nmattia1@gmail.com>
2026-05-28 19:36:56 +0200

    fix: add Terraform output for oauth integration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,6 +8,7 @@ output "provides" {
 
 output "requires" {
   value = {
+    oauth_jwt_issuer = "oauth-jwt-issuer"
     m2m_request_auth = "m2m-request-auth",
     ui_request_auth  = "ui-request-auth"
   }
