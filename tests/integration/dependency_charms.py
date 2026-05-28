49a7675ff2421d1c43710d9503423c350f1f82a0
MattiaSarti <nmattia1@gmail.com>
2026-05-28 19:28:25 +0200

    docs: improve comments

--- tests/integration/dependency_charms.py
+++ tests/integration/dependency_charms.py
@@ -2,7 +2,7 @@
 
 from charmed_kubeflow_chisme.testing import CharmSpec
 
-# for the identity prodiver:
+# for the identity provider yielding the JWT issuer:
 HYDRA = CharmSpec(charm="hydra", channel="latest/stable", trust=True, config={"dev": True})
 LOGIN_UI = CharmSpec(charm="identity-platform-login-ui-operator", channel="latest/stable", trust=True)
 POSTGRESQL = CharmSpec(
@@ -16,6 +16,6 @@ POSTGRESQL = CharmSpec(
 )
 TRAEFIK = CharmSpec(charm="traefik-k8s", channel="latest/stable", trust=True)
 
-# for the two Istio ingresses:
+# for the two Istio ingresses managing one respective RequestAuthentication resource each:
 ISTIO_INGRESS_K8S = CharmSpec(charm="istio-ingress-k8s", channel="dev/edge", trust=True)
 ISTIO_K8S = CharmSpec(charm="istio-k8s", channel="dev/edge", trust=True, config={"platform": ""})
